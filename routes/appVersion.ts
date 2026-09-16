e97d5d36dd980637701366540ee19e195207c8a1
Student <student@qwiklabs.net>
2026-09-16 18:09:40 +0000

    ci: trigger CodeMender CI/CD Guardrail workflow
    Signed-off-by: Student <student@qwiklabs.net>

--- routes/appVersion.ts
+++ routes/appVersion.ts
@@ -2,6 +2,8 @@
  * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
+// CI trigger comment
+
 
 import config from 'config'
 import { type Request, type Response } from 'express'
